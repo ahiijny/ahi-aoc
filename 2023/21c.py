662a389f1e4cb664c5d0d4ad3badaa35215dba9f
ahiijny <ahiijny@gmail.com>
2023-12-28 04:14:32 -0500

    loop tech

--- 2023/21c.py
+++ 2023/21c.py
@@ -29,7 +29,10 @@ adjs = [
 W = len(core_grid[0])
 H = len(core_grid)
 
-state_hist = {} # record duplicate states in repeated boards; 
+state_hist = {} # record duplicate states in repeated boards;
+
+def unconvert(u, p):
+	return (W * u[0] + p[0], H * u[1] + p[1])
 
 def convert(x, y):
 	"""
@@ -70,7 +73,7 @@ def add(a, b):
 state_to_i = {} # map from metaboard state to list of -> (metaboard coords, step) occurrences
 i_to_state = {}
 
-steps = 300
+steps = 150
 
 all_locs = set()
 all_locs.add(start)
@@ -97,9 +100,6 @@ maxx = start[0]
 miny = start[1]
 maxy = start[1]
 
-shockwave = set()
-shockwave.add(start)
-
 def print_metaboards():
 	minx = None
 	maxx = None
@@ -117,32 +117,52 @@ def print_metaboards():
 		for x in range(minx, maxx + 1):
 			print(f"{str(len(locs_by_board.get((x,y), set()))):<4}", end=' ')
 		print()
-			
-	
+
+looping_metaboards = {}
+
+def find_equiv_index(a, b, i):
+	"""
+	for a loop where [a, b] and state[a] === state[b],
+		e.g. 2<->5
+		2,3,4,5(2),6(3)
+	state is i % (b-a)
+	"""
+	if i < b:
+		return i
+	return i % (b-a) + a
 
 for i in range(steps):
 	next_locs = set()
-	
-	for loc in shockwave:
-		for adj in adjs:
-			p2 = add(loc, adj)
-			minx = min(minx, p2[0])
-			maxx = max(maxx, p2[0])
-			miny = min(miny, p2[1])
-			maxy = max(maxy, p2[1])
-			
-			u, ep = convert(*p2)
-			#print(f"{p2} -> universe = {u}, equiv_point = {p2}")
-			if u not in locs_by_board:
-				locs_by_board[u] = set()
-			locs_by_board[u].add(ep)
-			if p2 not in all_locs:
-				if grid(*p2) != '#':
-					next_locs.add(p2)
-	
-	shockwave = next_locs
-	all_locs.update(next_locs)
-	print(f"{i} steps: {len(locs_by_board)} metaboards, bounding box: {(minx, miny)} -> {(maxx, maxy)}, visited = {len(all_locs)}")
+	next_locs_by_board = {}
+	for u, locs in locs_by_board.items():
+		if u in looping_metaboards:
+			a,b = looping_metaboards[u]
+			i0 = find_equiv_index(a, b, i)
+			print(f"reusing looping metaboard {u}@{i0}")
+			sublocs = {unconvert(u, p) for p in i_to_state[i0][u]}
+			next_locs.update(sublocs)
+			next_locs_by_board[u] = sublocs
+		else:
+			for loc in locs:
+				loc = unconvert(u, loc)
+				for adj in adjs:
+					p2 = add(loc, adj)
+					minx = min(minx, p2[0])
+					maxx = max(maxx, p2[0])
+					miny = min(miny, p2[1])
+					maxy = max(maxy, p2[1])
+					
+					u2, ep = convert(*p2)
+					#print(f"{p2} -> universe = {u2}, equiv_point = {p2}")
+					if u2 not in next_locs_by_board:
+						next_locs_by_board[u2] = set()
+					if p2 not in all_locs:
+						if grid(*p2) != '#':
+							next_locs_by_board[u2].add(ep)
+							next_locs.add(p2)
+	all_locs = next_locs
+	locs_by_board = next_locs_by_board
+	print(f"{i} steps: {len(locs_by_board)} metaboards, bounding box: {(minx, miny)} -> {(maxx, maxy)}, reachable = {len(all_locs)}")
 	
 	print_metaboards()
 	
@@ -157,15 +177,21 @@ for i in range(steps):
 		if state in state_hist:
 			u_to_iis = state_hist[state]
 			u2, i2 = next(iter(u_to_iis.items()))
+			statestr = "" if len(state) > 5 else str(state)
 			if u not in u_to_iis:
-				print(f"recurring state: metaboard {u}@{i} steps matches metaboard {u2}@{i2} steps")
-				u_to_iis[u] = i
+				u_to_iis[u] = []
+			if len(u_to_iis[u]) < 2:
+				print(f"recurring state: metaboard {u}@{i} steps matches metaboard {u2}@{i2} steps: {statestr}")
+				u_to_iis[u].append(i)
+				if len(u_to_iis[u]) == 2:
+					print(f"stable loop found for metaboard {u}")
+					looping_metaboards[u] = tuple(u_to_iis[u])
+			
 		else:
-			state_hist[state] = {u: i}
+			state_hist[state] = {u: [i]}
 	
-	if i % 8 == 1:
-		pass
-		# print_grid(i + 4)
+	if len(all_locs) < 1000:
+		print_grid(i + 4)
 	
 
 print(f"num visited: {len(all_locs)}")
